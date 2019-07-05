0098d075abbe2517c5c40ee7460db65e2f12a859
Steven Loria <sloria1@gmail.com>
2019-07-05 10:49:38 -0400

    Update pre-commit and use official flake8 hooks

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ EXTRAS_REQUIRE = {
     "examples": ["httpie", "uvicorn"],
     "lint": [
         "mypy==0.650",
-        "flake8==3.6.0",
-        "flake8-bugbear==18.8.0",
-        "pre-commit==1.13.0",
+        "flake8==3.7.7",
+        "flake8-bugbear==19.3.0",
+        "pre-commit~=1.17",
     ],
 }
 EXTRAS_REQUIRE["dev"] = (
