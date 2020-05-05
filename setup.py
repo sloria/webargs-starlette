8c9d16061563d6ba1d852ed00bb2ce6b937ad855
Steven Loria <sloria1@gmail.com>
2020-05-05 00:13:52 -0400

    Update classifiers

--- setup.py
+++ setup.py
@@ -65,6 +65,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Internet :: WWW/HTTP",
     ],
     test_suite="tests",
